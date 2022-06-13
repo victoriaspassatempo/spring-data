e00cb85a2e39f061e0aa5c356f580546cbb2c3cf
Victoria Rocha <victoriaspassatempo@gmail.com>
2022-06-13 10:56:46 -0300

    Add files via upload
    Adição do projeto

---
+++ primeiro-projeto-spring/primeiro-projeto-spring/src/main/java/com/springdata/primeiroprojetospring/PrimeiroProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.springdata.primeiroprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ primeiro-projeto-spring/primeiro-projeto-spring/src/test/java/com/springdata/primeiroprojetospring/PrimeiroProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.primeiroprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
