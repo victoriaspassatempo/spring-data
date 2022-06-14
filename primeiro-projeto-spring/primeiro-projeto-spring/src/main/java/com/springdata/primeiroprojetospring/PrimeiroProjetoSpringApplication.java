b18817ca0acad42e5e7e47c92925edb940945328
Victoria <victoriaspassatempo@gmail.com>
2022-06-14 15:04:33 -0300

    Adição de mensagem

--- primeiro-projeto-spring/primeiro-projeto-spring/src/main/java/com/springdata/primeiroprojetospring/PrimeiroProjetoSpringApplication.java
+++ primeiro-projeto-spring/primeiro-projeto-spring/src/main/java/com/springdata/primeiroprojetospring/PrimeiroProjetoSpringApplication.java
@@ -11,5 +11,6 @@ public class PrimeiroProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoSpringApplication.class, args);
+		System.out.println("Aplicação esta no ar!");
 	}
 }
